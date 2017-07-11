d0f7ae4e314943bf61d137c53532c6b09ca9f67d
fuchenxi <fuchenxi@floruit.cn>
2017-07-11 11:09:47 +0800

    初始化项目

---
+++ FCParallaxEffectViewController/FCParallaxEffectViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FCParallaxEffectViewController
+//
+//  Created by 付晨曦 on 2017/7/11.
+//  Copyright © 2017年 付晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FCParallaxEffectViewController/FCParallaxEffectViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FCParallaxEffectViewController
+//
+//  Created by 付晨曦 on 2017/7/11.
+//  Copyright © 2017年 付晨曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
